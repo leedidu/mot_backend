cca87aec93b9316c1a39fbbc24975967479c1e02
jihyun <jihuyn3066@naver.com>
2023-08-01 18:38:49 +0900

    feat -#1 : 기초 CRUD

--- src/main/java/com/umc/mot/sellMember/service/SellMemberService.java
+++ src/main/java/com/umc/mot/sellMember/service/SellMemberService.java
@@ -2,7 +2,7 @@ package com.umc.mot.sellMember.service;
 
 import com.umc.mot.exception.BusinessLogicException;
 import com.umc.mot.exception.ExceptionCode;
-import com.umc.mot.sellMember.entity.SellMemberEntity;
+import com.umc.mot.sellMember.entity.SellMember;
 import com.umc.mot.sellMember.repository.SellMemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,21 +16,21 @@ public class SellMemberService {
     private final SellMemberRepository sellMemberRepository;
 
     //Create
-    public SellMemberEntity createSellMember(SellMemberEntity sellmember) {
+    public SellMember createSellMember(SellMember sellmember) {
 
         return sellMemberRepository.save(sellmember);
     }
 
     // Read
-    public SellMemberEntity findMember(int sellMemberId) {
-        SellMemberEntity member = verifiedMember(sellMemberId);
+    public SellMember findMember(int sellMemberId) {
+        SellMember member = verifiedMember(sellMemberId);
         return member;
     }
 
 
     // Update
-    public SellMemberEntity patchMember(SellMemberEntity member) {
-        SellMemberEntity findMember = verifiedMember(member.getSellMemberId());
+    public SellMember patchMember(SellMember member) {
+        SellMember findMember = verifiedMember(member.getSellMemberId());
         Optional.ofNullable(member.getId()).ifPresent(findMember::setId);
         Optional.ofNullable(member.getName()).ifPresent(findMember::setName);
         Optional.ofNullable(member.get)
@@ -46,8 +46,8 @@ public class SellMemberService {
     }
 
     // 멤버 검증
-    public SellMemberEntity verifiedMember(int sellMemberId) {
-        Optional<SellMemberEntity> member = sellMemberRepository.findById(sellMemberId);
+    public SellMember verifiedMember(int sellMemberId) {
+        Optional<SellMember> member = sellMemberRepository.findById(sellMemberId);
         return member.orElseThrow(() -> new BusinessLogicException(ExceptionCode.SELL_MEMBER_NOT_FOUND));
 
     }

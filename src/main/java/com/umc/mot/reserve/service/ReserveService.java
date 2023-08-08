ef9a092e9b72af767074636bf02d4d63a456f671
dlwltn510 <dlwltn510@gmail.com>
2023-08-09 02:52:43 +0900

    feat - #12 : 나의 예약내역 확인 구현
    ReserveService 예약 정보 중 패키지/객실 정보

--- src/main/java/com/umc/mot/reserve/service/ReserveService.java
+++ src/main/java/com/umc/mot/reserve/service/ReserveService.java
@@ -3,6 +3,7 @@ package com.umc.mot.reserve.service;
 import com.umc.mot.exception.BusinessLogicException;
 import com.umc.mot.exception.ExceptionCode;
 import com.umc.mot.hotel.entity.Hotel;
+import com.umc.mot.hotel.service.HotelService;
 import com.umc.mot.packagee.entity.Package;
 import com.umc.mot.packagee.service.PackageService;
 import com.umc.mot.purchaseMember.entity.PurchaseMember;
@@ -15,6 +16,7 @@ import com.umc.mot.token.service.TokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.*;
 
 @Service
@@ -25,22 +27,17 @@ public class ReserveService {
     private final RoomService roomService;
     private final PackageService packageService;
     private final TokenService tokenService;
-
-    /*
-    * 첫번째 for : 구매자의 예약확인
-    * if문 안에 for : 각 방의 호텔정보 얻기
-    * 해서 각방의 호텔정보를 List<Hotel>안에 넣어서 반환
-    * */
+    private final HotelService hotelService;
 
     public List<Hotel> findHotels(){ // 예약된 호텔 리스트 찾기
         PurchaseMember purchaseMember = tokenService.getLoginPurchaseMember();
         List<Hotel> hotellist = new ArrayList();
         for(Reserve reserve : purchaseMember.getReserves()) { // 예약 된거 확인
-            if(reserve.getPackages() != null && !reserve.getPackages().equals("")){
+            if(reserve.getPackages() != null && !reserve.getPackages().isEmpty()){
                 for(Package packagee : reserve.getPackages()){
                     hotellist.add(packagee.getHotel());
                 }
-            } if(reserve.getRooms() != null && !reserve.getRooms().equals("")){
+            } if(reserve.getRooms() != null && !reserve.getRooms().isEmpty()){
                 for(Room room : reserve.getRooms()){
                     hotellist.add(room.getHotel());
                 }
@@ -49,12 +46,29 @@ public class ReserveService {
         return hotellist;
     }
 
-    public Map<Reserve, List<Room>> findRooms(){
+    public Map<Reserve, List<Room>> findRooms(){ //예약 정보 중 객실 정보
+        PurchaseMember purchaseMember = tokenService.getLoginPurchaseMember();
+        Map<Reserve, List<Room>> reserveRooms = new HashMap<>();
+        for(Reserve reserve : purchaseMember.getReserves()){
+            reserveRooms.put(reserve, reserve.getRooms());
+        }
+        return reserveRooms;
+    }
+
+    public Map<Reserve, List<Package>> findPackages(){ //예약 정보 중 패키지 정보
+        PurchaseMember purchaseMember = tokenService.getLoginPurchaseMember();
+        Map<Reserve, List<Package>> reservePackages = new HashMap<>();
+        for(Reserve reserve : purchaseMember.getReserves()){
+            reservePackages.put(reserve, reserve.getPackages());
+        }
+        return reservePackages;
+    }
+
+    public Hotel hotelInfo(){
+        PurchaseMember purchaseMember = tokenService.getLoginPurchaseMember();
 
         return null;
     }
-
-
 
     //Create
     public Reserve createReserve(Reserve reserve) {

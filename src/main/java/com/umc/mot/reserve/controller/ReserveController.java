758940906589f64979b62b91f0cd7e7257d9f39c
kung036 <sinariari3@gmail.com>
2023-08-12 08:42:50 +0900

    feat - #26: S3 이미지 삭제 구현중

--- src/main/java/com/umc/mot/reserve/controller/ReserveController.java
+++ src/main/java/com/umc/mot/reserve/controller/ReserveController.java
@@ -26,6 +26,7 @@ import java.util.List;
 public class ReserveController {
     private final ReserveService reserveService;
     private final ReserveMapper reserveMapper;
+    private TokenService tokenService;
 
     // Create
     @PostMapping

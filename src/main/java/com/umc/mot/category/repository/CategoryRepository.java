a785a1481aa39c82ccef10e5acbc7d213ba27dae
isoProject <jj99526@naver.com>
2023-08-20 23:11:14 +0900

    태그추가 및 날짜검색

--- src/main/java/com/umc/mot/category/repository/CategoryRepository.java
+++ src/main/java/com/umc/mot/category/repository/CategoryRepository.java
@@ -5,4 +5,5 @@ import com.umc.mot.heart.entity.Heart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    Category findByName(String categorylists);
 }
--- src/main/java/com/umc/mot/hotel/repository/HotelRepository.java
+++ src/main/java/com/umc/mot/hotel/repository/HotelRepository.java
@@ -5,12 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public interface HotelRepository extends JpaRepository<Hotel, Integer> {
-    @Query("SELECT h FROM Hotel h WHERE (h.name LIKE %:post% or h.address Like %:post%) and (h.minPeople<=:people and h.maxPeople>=:people)")
-    ArrayList<Hotel> findByName(String post,int people);
+    @Query("SELECT h FROM Hotel h  WHERE (h.name LIKE %:post% or h.address Like %:post%)")
+    ArrayList<Hotel> findByName(String post);
+
+    @Query("SELECT h FROM Hotel h left join Reserve r on h.id=r.hotel.id WHERE (h.name LIKE %:post% or h.address Like %:post%) and (h.minPeople<=:people and h.maxPeople>=:people) and (r.checkIn>=:checkout or r.checkOut<=:checkin or r.checkIn=null or r.checkOut=null)")
+        // @Query("SELECT h FROM Hotel h join Reserve r on h.id=r.hotel.id WHERE (h.name LIKE %:post% or h.address Like %:post%) and (h.minPeople<=:people and h.maxPeople>=:people)")
+    ArrayList<Hotel> findByPeopleDay(String post, LocalDate checkin, LocalDate checkout, int people);
 
 
 
